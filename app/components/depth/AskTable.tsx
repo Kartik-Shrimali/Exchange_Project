6095efe0b943710b92e7be7548ced27640a50a4b
Kartik-Shrimali <kartikshrimali62@gmail.com>
2026-05-13 16:50:28 +0530

    added depth bars to bids and ask tables

--- app/components/depth/AskTable.tsx
+++ app/components/depth/AskTable.tsx
@@ -1,10 +1,56 @@
-export default function AskTable({asks}: {asks: [string , string][]}){
+export default function AskTable({ asks }: { asks: [string, string][] }) {
+    let currentTotal: number = 0;
+    const releventAsks = asks.slice(0, 15);
+    releventAsks.reverse();
+    let asksWithTotal: [string, string, number][] = [];
+
+    for (const [price, quantity] of releventAsks) {
+        currentTotal = currentTotal + parseFloat(quantity);
+        asksWithTotal.push([price, quantity, currentTotal])
+    }
+
+    const maxTotal = releventAsks.reduce((acc, [price, quantity]) => acc += Number(quantity), 0)
+
     return <div>
         AskTable
-        {asks.map(([price, quantity] , index) => (
-            <p key = {index}>
-                Price: {price} || Quantity : {quantity}
-            </p>    
+        {asksWithTotal.map(([price, quantity, total], index) => (
+            // <p key = {index}>
+            //     Price: {price} || Quantity : {quantity} || Total: {total}
+            // </p>
+            <div key = {index}>
+                <div
+                    style={{
+                        display: "flex",
+                        position: "relative",
+                        width: "100%",
+                        backgroundColor: "transparent",
+                        overflow: "hidden",
+                    }}
+                >
+                    <div
+                        style={{
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                            width: `${(100 * total) / maxTotal}%`,
+                            height: "100%",
+                            background: "rgba(228, 75, 68, 0.325)",
+                            transition: "width 0.3s ease-in-out",
+                        }}
+                    ></div>
+                    <div className="flex justify-between text-xs w-full">
+                        <div>
+                            {price}
+                        </div>
+                        <div>
+                            {quantity}
+                        </div>
+                        <div>
+                            {total?.toFixed(2)}
+                        </div>
+                    </div>
+                </div>
+            </div>
         ))}
     </div>
 }
--- app/components/depth/BidTable.tsx
+++ app/components/depth/BidTable.tsx
@@ -1,11 +1,56 @@
-export default function BidTable({bids} : {bids : [string , string][]}){
+export default function BidTable({ bids }: { bids: [string, string][] }) {
+    let currentTotal: number = 0;
+    const releventBids = bids.slice(0, 15);
+    let bidsWithTotal: [string, string, number][] = [];
+
+    for (const [price, quantity] of releventBids) {
+        currentTotal = currentTotal + parseFloat(quantity);
+        bidsWithTotal.push([price, quantity, currentTotal])
+    }
+
+    const maxTotal = releventBids.reduce((acc, [price, quantity]) => acc += Number(quantity), 0);
+
     return (
         <div>
             BidTable
-            {bids.map(([price , quantity] , index) => (
-                <p key = {index}>
-                    Price: {price} || Quantity : {quantity}
-                </p>    
+            {bidsWithTotal.map(([price, quantity, total], index) => (
+                // <p key = {index}>
+                //     Price: {price} || Quantity : {quantity} || Total : {total}
+                // </p>
+                <div key = {index}>
+                    <div
+                        style={{
+                            display: "flex",
+                            position: "relative",
+                            width: "100%",
+                            backgroundColor: "transparent",
+                            overflow: "hidden",
+                        }}
+                    >
+                        <div
+                            style={{
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                width: `${(100 * total) / maxTotal}%`,
+                                height: "100%",
+                                background: "rgba(1, 167, 129, 0.325)",
+                                transition: "width 0.3s ease-in-out",
+                            }}
+                        ></div>
+                        <div className={`flex justify-between text-xs w-full`}>
+                            <div>
+                                {price}
+                            </div>
+                            <div>
+                                {quantity}
+                            </div>
+                            <div>
+                                {total.toFixed(2)}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             ))}
         </div>
     )
