9100f3a1082c16974e9ced5e1a099c85ff4c12d9
Kartik-Shrimali <kartikshrimali62@gmail.com>
2026-05-21 16:21:07 +0530

    completed depth and order routes

--- backend/src/api/routes/depth.ts
+++ backend/src/api/routes/depth.ts
@@ -1,9 +1,20 @@
 import express from "express"
+import { RedisManager } from "../RedisManager";
+import { GET_DEPTH } from "../../types/fromApi";
 
 const depthRouter = express.Router();
 
-depthRouter.get("/" , (req , res) =>{
-    
+depthRouter.get("/" , async (req , res) =>{
+    const symbol = req.query.symbol as string;
+    const redisInstance = RedisManager.getInstance();
+
+    const response = await redisInstance.sendAndAwait({
+        type : GET_DEPTH,
+        data : {market : symbol}
+    })
+    console.log("Depth response get /:" , response)
+
+    res.status(200).json(response)
 })
 
 export {depthRouter}
--- backend/src/api/routes/order.ts
+++ backend/src/api/routes/order.ts
@@ -1,15 +1,48 @@
 import express from "express"
+import { RedisManager } from "../RedisManager";
+import { CANCEL_ORDER, CREATE_ORDER, GET_OPEN_ORDERS } from "../../types/fromApi";
 
 const orderRouter = express.Router();
 
-orderRouter.post("/" , (req , res) => {
-    
-})
-orderRouter.delete("/" , (req , res) => {
+orderRouter.post("/", async (req, res) => {
+    const { market, price, quantity, side, userId } = req.body;
 
-})
-orderRouter.get("/open" , (req , res) => {
+    const redisInstance = RedisManager.getInstance()
 
+    const response = await redisInstance.sendAndAwait({
+        type: CREATE_ORDER,
+        data: { market, price, quantity, side, userId }
+    })
+    console.log("Order response post /:", response)
+    res.status(200).json(response)
 })
 
-export {orderRouter}
+orderRouter.delete("/", async (req, res) => {
+    const { orderId, market, userId } = req.body;
+
+    const redisInstance = RedisManager.getInstance();
+
+    const response = await redisInstance.sendAndAwait({
+        type: CANCEL_ORDER,
+        data: { orderId, market, userId }
+    })
+    console.log("Order response delete /:", response)
+
+    res.status(200).json(response);
+})
+orderRouter.get("/open", async (req, res) => {
+    const userId = req.query.userId as string;
+    const market = req.query.market as string;
+
+    const redisInstance = RedisManager.getInstance();
+
+    const response = await redisInstance.sendAndAwait({
+        type: GET_OPEN_ORDERS,
+        data: { userId, market }
+    })
+    console.log("Order response get /:", response)
+
+    res.status(200).json(response)
+})
+
+export { orderRouter }
